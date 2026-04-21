8785ee6982c6ada6b7a7fe2171cab4c39cfb2231
DDanielPE <jesusdanielpastorespinosa4@gmail.com>
2026-04-20 19:06:19 -0600

    MAIN

---
+++ Controlador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MAIN;
+
+import GUI.Sistem;
+
+/**
+ *
+ * @author soyfe
+ */
+
+    public class Controlador {
+    public static void main(String[] args) {
+        new Sistem().setVisible(true);
+    }
+}
+
+
